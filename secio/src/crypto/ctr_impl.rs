0c7e689f943388251f0c4d7e524d499e5f33c9a8
daijianwei <742537019@qq.com>
2020-08-13 15:55:33 +0800

    remove unused dependencies

--- secio/src/crypto/ctr_impl.rs
+++ secio/src/crypto/ctr_impl.rs
@@ -5,7 +5,6 @@ use crate::error::SecioError;
 use aes_ctr::stream_cipher::generic_array::*;
 use aes_ctr::stream_cipher::{NewStreamCipher, SyncStreamCipher};
 use aes_ctr::Aes128Ctr;
-use log::trace;
 
 pub static CTR128LEN: usize = 16;
 
