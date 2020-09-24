a590c8447535e07a28c070c1072c61b75f872da0
Kingwel Xie <kingwel.xie@139.com>
2020-09-24 09:03:19 +0800

    swarm/protocl-handler: cleanup unused files

--- swarm/src/protocol_handler/dummy.rs
+++ swarm/src/protocol_handler/dummy.rs
@@ -17,39 +17,3 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
-
-use async_trait::async_trait;
-use libp2p_core::upgrade::{UpgradeInfo, ProtocolName};
-use crate::protocol_handler::{ProtocolHandler, BoxHandler};
-use crate::SwarmError;
-
-/// Implementation of `ProtocolHandler` that doesn't handle anything.
-#[derive(Clone, Default)]
-pub struct DummyProtocolHandler {
-}
-
-impl DummyProtocolHandler {
-    pub fn new() -> Self {
-        DummyProtocolHandler {
-        }
-    }
-}
-
-impl UpgradeInfo for DummyProtocolHandler {
-    type Info = &'static [u8];
-    fn protocol_info(&self) -> Vec<Self::Info> {
-        vec![b"/dummy/1.0.0", b"/dummy/2.0.0"]
-    }
-}
-
-#[async_trait]
-impl<C: Send + std::fmt::Debug + 'static> ProtocolHandler<C> for DummyProtocolHandler {
-
-    async fn handle(&mut self, stream: C, info: <Self as UpgradeInfo>::Info) -> Result<(), SwarmError> {
-        log::trace!("Dummy Protocol handling inbound {:?} {:?}", stream, info.protocol_name_str());
-        Ok(())
-    }
-    fn box_clone(&self) -> BoxHandler<C> {
-        Box::new(self.clone())
-    }
-}
