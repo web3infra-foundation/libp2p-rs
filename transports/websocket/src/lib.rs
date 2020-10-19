fed91dcfb53b7bfebd40701ad894772de558879c
kuang <3168582@qq.com>
2020-10-19 16:49:00 +0800

    fix port conflict

--- transports/websocket/src/lib.rs
+++ transports/websocket/src/lib.rs
@@ -141,8 +141,8 @@ mod tests {
     #[test]
     fn dialer_connects_to_listener_dns() {
         //env_logger::from_env(env_logger::Env::default().default_filter_or("debug")).init();
-        let listen_addr = "/ip4/127.0.0.1/tcp/38099/ws".parse().unwrap();
-        let dial_addr = "/dns4/localhost/tcp/38099/ws".parse().unwrap();
+        let listen_addr = "/ip4/127.0.0.1/tcp/38100/ws".parse().unwrap();
+        let dial_addr = "/dns4/localhost/tcp/38100/ws".parse().unwrap();
         let s = task::spawn(async {
             server(listen_addr).await;
         });
@@ -157,8 +157,8 @@ mod tests {
     #[test]
     fn dialer_connects_to_listener_ipv6() {
         //env_logger::from_env(env_logger::Env::default().default_filter_or("debug")).init();
-        let listen_addr = "/ip6/::1/tcp/38088/ws".parse().unwrap();
-        let dial_addr = "/ip6/::1/tcp/38088/ws".parse().unwrap();
+        let listen_addr = "/ip6/::1/tcp/38101/ws".parse().unwrap();
+        let dial_addr = "/ip6/::1/tcp/38101/ws".parse().unwrap();
         let s = task::spawn(async {
             server(listen_addr).await;
         });
