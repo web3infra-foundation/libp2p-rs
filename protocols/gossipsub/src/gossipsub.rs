f99ffa9720328696e1fadb6ede736834eb7bec21
restoreall <742537019@qq.com>
2021-06-28 12:07:02 +0800

    gossip: add copyright

--- protocols/gossipsub/src/gossipsub.rs
+++ protocols/gossipsub/src/gossipsub.rs
@@ -1,4 +1,5 @@
 // Copyright 2020 Sigma Prime Pty Ltd.
+// Copyright 2020 Netwarps Ltd.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
