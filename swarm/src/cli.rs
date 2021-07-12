a36702e314a52f857d0cc5b9830a5cdf46b94d3f
kingwel <kingwel.xie@139.com>
2021-07-12 15:26:42 +0800

    swarm/cli: shows the total connection/stream

--- swarm/src/cli.rs
+++ swarm/src/cli.rs
@@ -174,6 +174,7 @@ fn cli_show_connections(app: &App, args: &[&str]) -> XcliResult {
 
     task::block_on(async {
         let connections = swarm.dump_connections(peer).await.unwrap();
+        let mut ss = 0;
         println!("CID   DIR Remote-Peer-Id                                       I/O Local-Multiaddr                  Remote-Multiaddr                 Latency");
         connections.iter().for_each(|v| {
             println!(
@@ -194,7 +195,10 @@ fn cli_show_connections(app: &App, args: &[&str]) -> XcliResult {
                     println!("      ({})", s);
                 });
             }
+
+            ss += v.substreams.len();
         });
+        println!("Total {} connections, {} sub-streams", connections.len(), ss);
     });
 
     Ok(CmdExeCode::Ok)
