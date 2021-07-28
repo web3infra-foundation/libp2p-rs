261111d64b8ae6d2ed7724140c2e157619126d52
itling <3168582@qq.com>
2021-07-28 20:27:30 +0800

    kad: add env param

--- protocols/kad/src/kad.rs
+++ protocols/kad/src/kad.rs
@@ -1513,10 +1513,21 @@ where
         self.messengers = Some(MessengerManager::new(swarm.clone(), self.protocol_config.clone()));
         self.swarm = Some(swarm);
 
-        // start provider gc timer
-        self.start_provider_gc_timer();
-        // start refresh timer
-        self.start_refresh_timer();
+       // start provider gc timer
+       let gc_timer = std::env::var_os("KAD_GC_TIMER")
+       .map(|i| i.into_string().unwrap().parse::<bool>().unwrap())
+       .unwrap_or(true);
+       if(gc_timer){
+           self.start_provider_gc_timer();
+       }
+
+       // start refresh timer
+       let refresh_timer = std::env::var_os("KAD_REFRESH_TIMER")
+       .map(|i| i.into_string().unwrap().parse::<bool>().unwrap())
+       .unwrap_or(true);
+       if(refresh_timer){
+           self.start_refresh_timer();
+       }
 
         // well, self 'move' explicitly,
         let mut kad = self;
